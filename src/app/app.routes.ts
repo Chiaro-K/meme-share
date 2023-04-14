62a28c07d8aac43b2429d8124148cb60b98a4036
altc_khanyiso <khanyiso.mapuma@altcom.co.uk>
2023-04-14 17:30:32 +0200

    home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,13 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
   },
+  {
+    path: 'search',
+    loadComponent: () => import('./search/search.page').then( m => m.SearchPage)
+  },
+
 ];
