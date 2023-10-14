9325687dccfda231f8b4437513ecb8745501c985
Khanyiso Mapuma <mapumak@gmail.com>
2023-10-14 09:03:38 +0200

    clean up

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,15 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import {
-  IonModal,
-  IonicModule,
-  ModalController,
-  NavController,
-  NavParams,
-} from '@ionic/angular';
-import { AnimationController } from '@ionic/angular';
+import { IonicModule, ModalController, NavParams } from '@ionic/angular';
 import { IPost } from 'src/app/models/Post';
 import { PostType } from 'src/app/models/PostTypes';
 import { PostService } from 'src/app/services/posts/postService';
@@ -24,13 +16,12 @@ import { Capitalize } from 'src/app/shared/stringUtils';
   imports: [IonicModule, CommonModule],
 })
 export class PostComponent implements OnInit {
-  post: IPost;
-  posts: IPost[] | undefined;
+  post?: IPost;
+  posts?: IPost[];
   user: any;
   userPostCount: number = 0;
 
   constructor(
-    private animationCtrl: AnimationController,
     public modalCtrl: ModalController,
     private navParams: NavParams,
     public router: Router,
@@ -38,67 +29,42 @@ export class PostComponent implements OnInit {
     private userService: UserService
   ) {
     this.post = this.navParams.get('post');
-    console.log(this.post);
-    this.postService.incrementPostView(this.post.postId);
-    this.getPosts();
 
-    this.userService.getUser(this.post['userId']).then((res: any) => {
+    this.post = this.post;
+    this.getPosts();
+    this.postService.incrementPostView(this.post.postId);
+
+    this.userService.getUser(this.post.userId).then((res: any) => {
       console.log(res);
       this.user = res.data;
       this.userPostCount = this.user.posts?.length;
-    })
+    });
   }
 
-  viewPost(post: IPost){
+  viewPost(post: IPost) {
     this.modalCtrl.dismiss(post);
   }
 
   viewUser() {
-    this.router.navigate(['/view-user'], {
-      queryParams: {
-        userId: this.post.userId,
-        postId: this.post.postId,
-      },
-    });
+    if (this.post) {
+      this.router.navigate(['/view-user'], {
+        queryParams: {
+          userId: this.post.userId,
+          postId: this.post.postId,
+        },
+      });
+    }
   }
 
   getPosts() {
     this.postService
-      .getAllPosts(Capitalize(PostType[this.post.postType]))
+      .getAllPosts(Capitalize(PostType[this.post!.postType]))
       .then((res) => {
         console.log('res', res);
         const posts = res.data;
-        this.posts = posts.filter((f: IPost) => f.postId !== this.post.postId);
+        this.posts = posts.filter((f: IPost) => f.postId !== this.post!.postId);
       });
   }
-
-  // enterAnimation = (baseEl: HTMLElement) => {
-  //   const root = baseEl.shadowRoot;
-
-  //   const backdropAnimation = this.animationCtrl
-  //     .create()
-  //     .addElement(root!.querySelector('ion-backdrop')!)
-  //     .fromTo('opacity', '0.01', 'var(--backdrop-opacity)');
-
-  //   const wrapperAnimation = this.animationCtrl
-  //     .create()
-  //     .addElement(root!.querySelector('.modal-wrapper')!)
-  //     .keyframes([
-  //       { offset: 0, opacity: '0', transform: 'scale(0)' },
-  //       { offset: 1, opacity: '0.99', transform: 'scale(1)' },
-  //     ]);
-
-  //   return this.animationCtrl
-  //     .create()
-  //     .addElement(baseEl)
-  //     .easing('ease-out')
-  //     .duration(150)
-  //     .addAnimation([backdropAnimation, wrapperAnimation]);
-  // };
-
-  // leaveAnimation = (baseEl: HTMLElement) => {
-  //   return this.enterAnimation(baseEl).direction('reverse');
-  // };
 
   ngOnInit() {}
 
